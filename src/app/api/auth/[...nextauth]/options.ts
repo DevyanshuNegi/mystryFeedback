8126a35898f99bfd3a54e2a39f2a4a2729439aaf
Devyanshu Negi <devyanshunegi@gmail.com>
2024-10-03 08:36:04 +0000

    understand about next auth and options.ts file working and types/next-auth.d.ts file

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,12 +7,14 @@ import UserModel from '@/model/User';
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
+      // id is not there in docs but used mostly.
       id: 'credentials',
       name: 'Credentials',
       credentials: {
         email: { label: 'Email', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
+      // this function is very imp otherwise throw error
       async authorize(credentials: any): Promise<any> {
         await dbConnect();
         try {
@@ -33,20 +35,34 @@ export const authOptions: NextAuthOptions = {
             user.password
           );
           if (isPasswordCorrect) {
-            return user;
+            return user; // return user or error
           } else {
             throw new Error('Incorrect password');
           }
         } catch (err: any) {
+          //NOTE: here throwig error is very important or return null;
           throw new Error(err);
         }
       },
     }),
   ],
   callbacks: {
+    // NOTE: here customising callbacks is must
+    // strictly follow docs and reutrn as specified in callbacks  
+    // like reutrn token for jwt, return sesion for session, return baseUrl for redirect
+
+    // here we only modify jwt and session but why ?
+    // 
     async jwt({ token, user }) {
+      // Making our token more powerfull
+      // here the user is the one which is returned by the previous function
       if (user) {
+        // NOTE: here we try to put max data into the token 
+        // this could increase payload size but will prevent many db calls
+
         token._id = user._id?.toString(); // Convert ObjectId to string
+        // here user._id will give error because ts only know about the basic user of next
+        // So we need to declare our user inside the types types folder  
         token.isVerified = user.isVerified;
         token.isAcceptingMessages = user.isAcceptingMessages;
         token.username = user.username;
@@ -68,6 +84,8 @@ export const authOptions: NextAuthOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
+    // NOTE: this is to override sign-in aroute or api 
+    // this will be handeled and made by next auth only
     signIn: '/sign-in',
   },
 };
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,5 +1,8 @@
 import 'next-auth';
 
+// NOTE: 10 here we are changing the User inside next-auth 
+// so we need to import the module and then change 
+// here we are changing or declaring the interface of next-auth
 declare module 'next-auth' {
   interface Session {
     user: {
@@ -8,6 +11,7 @@ declare module 'next-auth' {
       isAcceptingMessages?: boolean;
       username?: string;
     } & DefaultSession['user'];
+    // not sure this default is required or not
   }
 
   interface User {
